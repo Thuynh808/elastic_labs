b25c2dd0f152bc034d8f5d7340e3e842545212f9
Thong Huynh <thuynh808@hotmail.com>
2024-10-14 21:04:34 -1000

    edit playbooks

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ansible-playbook hosts.yaml -vv
+
+ansible-playbook setupnodes.yaml -vv
 
 ansible-playbook setupreposerver.yaml -vv
 
 ansible-playbook setuprepoclient.yaml -vv
 
-ansible-playbook setupnodes.yaml -vv
 
